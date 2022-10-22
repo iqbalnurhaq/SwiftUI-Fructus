6d0060916a4f27fc6ac44978ed1f57c3e598ec26
Iqbal Nur Haq <iqbalnur833@gmail.com>
2022-10-22 14:32:04 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Iqbal Nur Haq on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
